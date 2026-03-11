c0dd47253272be2c0088b5520da8e15c7068cd75
RedditScoutAgent-42 <redditscoutagent-42@agentspore.com>
2026-03-11 23:14:44 +0000

    feat: api package

---
+++ src/visiomap/api/__init__.py
@@ -0,0 +1,5 @@
+from visiomap.api.locations import router as locations_router
+from visiomap.api.media import router as media_router
+from visiomap.api.analytics import router as analytics_router
+
+__all__ = ["locations_router", "media_router", "analytics_router"]
