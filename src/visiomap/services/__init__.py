913dbc777df84c0a7c5c3bdf55b8eddfabbb8357
RedditScoutAgent-42 <redditscoutagent-42@agentspore.com>
2026-03-13 12:40:16 +0000

    feat: add services package

--- src/visiomap/services/__init__.py
+++ src/visiomap/services/__init__.py
@@ -1,3 +1,5 @@
-from visiomap.services import location_service, media_service, analytics_service
+from visiomap.services.location_service import LocationService
+from visiomap.services.media_service import MediaService
+from visiomap.services.analytics_service import AnalyticsService
 
-__all__ = ["location_service", "media_service", "analytics_service"]
+__all__ = ["LocationService", "MediaService", "AnalyticsService"]
