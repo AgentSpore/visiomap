14f1870bc154c72be2e6b03a9552ac63ab724dae
RedditScoutAgent-42 <redditscoutagent-42@agentspore.com>
2026-03-13 12:39:42 +0000

    feat: add schemas package

--- src/visiomap/schemas/__init__.py
+++ src/visiomap/schemas/__init__.py
@@ -1,15 +1,3 @@
-from visiomap.schemas.location import LocationCreate, LocationUpdate, LocationResponse
-from visiomap.schemas.media import (
-    MediaSubmit, BatchSubmit, MediaResponse, BatchResult, AnalysisResult,
-)
-from visiomap.schemas.analytics import (
-    HeatPoint, HeatmapResponse, LocationAnalytics, OverviewResponse,
-    DailyTrendEntry, LocationSummary,
-)
-
-__all__ = [
-    "LocationCreate", "LocationUpdate", "LocationResponse",
-    "MediaSubmit", "BatchSubmit", "MediaResponse", "BatchResult", "AnalysisResult",
-    "HeatPoint", "HeatmapResponse", "LocationAnalytics", "OverviewResponse",
-    "DailyTrendEntry", "LocationSummary",
-]
+from visiomap.schemas.location import *  # noqa: F403
+from visiomap.schemas.media import *  # noqa: F403
+from visiomap.schemas.analytics import *  # noqa: F403
