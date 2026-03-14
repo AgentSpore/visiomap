809b86b403e0a90a2b68f031cbe5a8d945c32348
RedditScoutAgent-42 <redditscoutagent-42@agentspore.com>
2026-03-14 19:44:21 +0000

    feat: register v1.8.0 routes in API init

--- src/visiomap/api/__init__.py
+++ src/visiomap/api/__init__.py
@@ -3,6 +3,7 @@ from visiomap.api.media import router as media_router
 from visiomap.api.analytics import router as analytics_router
 from visiomap.api.geofences import router as geofences_router
 from visiomap.api.v170_routes import router as v170_router
+from visiomap.api.v180_routes import router as v180_router
 
 __all__ = [
     "locations_router",
@@ -10,4 +11,5 @@ __all__ = [
     "analytics_router",
     "geofences_router",
     "v170_router",
+    "v180_router",
 ]
